41e8f4887f385a018d45636106702a11e3d080cc
stalky <zeyadalaaeldin20@gmail.com>
2023-11-06 23:31:11 +0100

    - Back-End: AuthN, AuthZ, Review System

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -6,11 +6,17 @@ import {
 } from "../Controllers/doctorController.js";
 import express from "express";
 
+import { authenticate, restrict } from "../auth/verifyToken.js";
+import reviewRouter from "./review.js";
+
 const router = express.Router();
+
+// nested route
+router.use("/:doctorId/reviews", reviewRouter);
 
 router.get("/:id", getSingleDoctor);
 router.get("/", getAllDoctor);
-router.put("/:id", updateDoctor);
-router.delete("/:id", deleteDoctor);
+router.put("/:id", authenticate, restrict(["doctor"]), updateDoctor);
+router.delete("/:id", authenticate, restrict(["doctor"]), deleteDoctor);
 
 export default router;
